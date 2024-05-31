85fb2371b779e06acee2937a478003e307e80668
Osama Hussein <osamahusseinn2@gmail.com>
2024-05-31 15:13:02 -0400

    The SFX and music buttons work in the options menu

--- Bee Game/Assets/Scripts/OptionsMenu.cs
+++ Bee Game/Assets/Scripts/OptionsMenu.cs
@@ -14,25 +14,23 @@ public class OptionsMenu : MonoBehaviour
     int screenWidth = 256;
     int screenHeight = 240;
 
-    // Create a public reference for sfx and music sliders for the player to play around with
-    public Slider sfxVolumeSlider;
-    public Slider musicVolumeSlider;
-
     // Create a public reference to any sfx and music audio files we have inside the game for the player to modify its volume
     public AudioSource[] sfxSource;
-    public AudioSource musicSource;
-
-    // Create a public reference to the sfx and music volume values text to display it to the player as they modify the slider
-    public TextMeshProUGUI sfxVolumeValue;
-    public TextMeshProUGUI musicVolumeValue;
-
-    // Create a float that converts the max slider value of 1 to a percent of 100
-    float percentage = 100.0f;
+    public AudioSource[] musicSource;
 
     int sfxIndex; // This will randomize a certain SFX to play
+    int musicIndex; // This will randomize a certain music to play
 
     float timerValue; // This is used for determining the time of the SFX index updating
-    float timerTarget = 0.5f; // This is used to reset the timer value back to 0 to keep updating the SFX index
+    float timerTarget = 1.0f; // This is used to reset the timer value back to 0 to keep updating the SFX index
+
+    // Create 2 images of SFX and no SFX images to use when the player presses the SFX button
+    public Image sfxImage;
+    public Image noSFXimage;
+
+    // Create 2 images of music and no music images to use when the player presses the music button
+    public Image musicImage;
+    public Image noMusicImage;
 
     // Start is called before the first frame update
     void Start()
@@ -40,40 +38,22 @@ public class OptionsMenu : MonoBehaviour
         // Set the resolution to the screen width and screen height
         Screen.SetResolution(screenWidth, screenHeight, fullScreenMode);
 
-        // Initialize the sfx and music slider values equal to their max values
-        sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFX volume");
-        musicVolumeSlider.value = PlayerPrefs.GetFloat("Music volume");
+        noSFXimage.gameObject.SetActive(false); // Turn off the no SFX image by default
+        sfxImage.gameObject.SetActive(true); // Turn on SFX at start
+
+        noMusicImage.gameObject.SetActive(false); // Turn off the no music image by default
+        musicImage.gameObject.SetActive(true); // Turn on music at start
     }
 
     // Update is called once per frame
     void Update()
     {
-        sfxIndex = Random.Range(0, 2);
+        // Set the SFX and music indexes to randomize between its first index and 1 more than the length of the array
+        sfxIndex = Random.Range(0, sfxSource.Length + 1);
+        musicIndex = Random.Range(0, musicSource.Length + 1);
 
+        // Increase the timer value over time
         timerValue += Time.deltaTime;
-
-        // Create local variables taking in the sfx volume percentage and music volume percentage
-        float sfxVolumePercentage = Mathf.Round(sfxVolumeSlider.value * percentage);
-        float musicVolumePercentage = Mathf.Round(musicVolumeSlider.value * percentage);
-
-        // Write a text of the sfx volume and music volume percentages inside the options menu
-        sfxVolumeValue.text = sfxVolumePercentage.ToString() + "%";
-        musicVolumeValue.text = musicVolumePercentage.ToString() + "%";
-
-        // If the sfx slider volume is at 0, make the sfx audio volume to 0 as well
-        if (sfxVolumeSlider.value <= 0)
-        {
-            for (int i = 0; i < sfxSource.Length; i++)
-            {
-                sfxSource[i].volume = 0;
-            }
-        }
-
-        // If the music slider volume is at 0, make the music audio volume to 0 as well
-        if (musicVolumeSlider.value <= 0)
-        {
-            musicSource.volume = 0;
-        }
     }
 
     public void PressBackButton()
@@ -82,42 +62,101 @@ public class OptionsMenu : MonoBehaviour
         SceneManager.LoadScene("Main Menu");
     }
 
-    public void ModifySFXVolume()
+    public void TurnOnSFX()
     {
-        // Change the sfx volume using the slider
-        for (int i = 0; i < sfxSource.Length; i++)
-        {
-            PlayerPrefs.SetFloat("SFX volume", sfxSource[i].volume = sfxVolumeSlider.value);
-        }
+        // Disable the no SFX image
+        noSFXimage.gameObject.SetActive(false);
 
-        // If the timer value is greater than the target time, use the sfx index to randomize playing between the SFXs
+        // Use the SFX image to tell the player they turned SFX on
+        sfxImage.gameObject.SetActive(true);
+
+        // If the timer is greater than its target, play the SFX at a specific index and reset the timer again
         if (timerValue > timerTarget)
         {
             if (sfxIndex == 0)
             {
-                sfxSource[0].Play();
+                sfxSource[0].volume = 1; // Set this SFX volume to 1
+                sfxSource[0].Play(); // Play this SFX at this SFX index
 
                 timerValue = 0; // Reset this to 0 to keep updating the sfx index
             }
 
             if (sfxIndex == 1)
             {
-                sfxSource[1].Play();
+                sfxSource[1].volume = 1; // Set this SFX volume to 1
+                sfxSource[1].Play(); // Play this SFX at this SFX index
 
                 timerValue = 0; // Reset this to 0 to keep updating the sfx index
             }
         }
     }
 
-    public void ModifyMusicVolume()
+    public void TurnOffSFX()
     {
-        // Change the music volume using the slider
-        PlayerPrefs.SetFloat("Music volume", musicSource.volume = musicVolumeSlider.value);
+        // Use the no SFX image to tell the player they turned SFX off
+        noSFXimage.gameObject.SetActive(true);
 
-        // Stop the SFX audio from playing when the player modifies the music slider
+        // Disable the SFX image
+        sfxImage.gameObject.SetActive(false);
+
+        // Loop through all the sfx sources
         for (int i = 0; i < sfxSource.Length; i++)
         {
-            sfxSource[i].Stop();
+            sfxSource[1].volume = 0; // Set all SFX volume to 0
+            sfxSource[i].Stop(); // Stop all SFXs from playing
+        }
+    }
+
+    public void TurnOnMusic()
+    {
+        // Disable the no music image
+        noMusicImage.gameObject.SetActive(false);
+
+        // Use the music image to indicate to the player that they turned on music
+        musicImage.gameObject.SetActive(true);
+
+        // If the timer is greater than its target, play the music at a specific index and reset the timer again
+        if (timerValue > timerTarget)
+        {
+            if (musicIndex == 0)
+            {
+                musicSource[0].volume = 1; // Set this music volume to 1 if it reaches this music index
+                musicSource[0].Play(); // Play this music when the player turns on music
+
+                timerValue = 0; // Reset this to 0 to keep updating the sfx index
+            }
+
+            if (musicIndex == 1)
+            {
+                musicSource[1].volume = 1; // Set this music volume to 1 if it reaches this music index
+                musicSource[1].Play(); // Play this music when the player turns on music
+
+                timerValue = 0; // Reset this to 0 to keep updating the sfx index
+            }
+
+            if (musicIndex == 2)
+            {
+                musicSource[2].volume = 1; // Set this music volume to 1 if it reaches this music index
+                musicSource[2].Play(); // Play this music when the player turns on music
+
+                timerValue = 0; // Reset this to 0 to keep updating the sfx index
+            }
+        }
+    }
+
+    public void TurnOffMusic()
+    {
+        // Use the no music image to indicate that the player turned off music
+        noMusicImage.gameObject.SetActive(true);
+
+        // Disable the music image
+        musicImage.gameObject.SetActive(false);
+
+        // Loop through all the music sources
+        for (int i = 0; i < musicSource.Length; i++)
+        {
+            musicSource[i].volume = 0; // Set all music volume to 0
+            musicSource[i].Stop(); // Stop all SFXs from playing
         }
     }
 }
