c8050556148e8ff9a37d7ed0a849f48f94043f70
Osama Hussein <osamahusseinn2@gmail.com>
2024-06-05 21:47:42 -0400

    Added texts inside the How to Play menu to tell the player what the different buttons do in-game

--- Bee Game/Assets/Scripts/HowToPlay.cs
+++ Bee Game/Assets/Scripts/HowToPlay.cs
@@ -9,6 +9,12 @@ public class HowToPlay : MonoBehaviour
     public Text backText; // Create a back button text to use inside the inspector
     public Font howToPlayFont; // Create a font to use inside the inspector
 
+    public Text dPadHelpText;
+    public Text startButtonHelpText;
+    public Text selectButtonHelpText;
+    public Text aButtonHelpText;
+    public Text bButtonHelpText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,5 +38,29 @@ public class HowToPlay : MonoBehaviour
             // Go back to the main menu only if the curtains have opened where the back button text is fully visible
             SceneManager.LoadScene("Main Menu");
         }
+
+        dPadHelpText.text = "Move the pointer around"; // Tell the player what the D-Pad buttons do
+        dPadHelpText.font = howToPlayFont; // Set this text to use the Nintendo font in the inspector
+        dPadHelpText.fontSize = 6; // Make the text smaller to fit the NES resolution
+        dPadHelpText.color = Color.white; // Make the text white
+        dPadHelpText.alignment = TextAnchor.MiddleLeft;// Align it at the middle left of the text box
+
+        selectButtonHelpText.text = "Open the building menu"; // Tell the player what the SELECT button does
+        selectButtonHelpText.font = howToPlayFont; // Set this text to use the Nintendo font in the inspector
+        selectButtonHelpText.fontSize = 6; // Make the text smaller to fit the NES resolution
+        selectButtonHelpText.color = Color.white; // Make the text white
+        selectButtonHelpText.alignment = TextAnchor.MiddleLeft;// Align it at the middle left of the text box
+
+        aButtonHelpText.text = "Collect resource"; // Tell the player what the A button does
+        aButtonHelpText.font = howToPlayFont; // Set this text to use the Nintendo font in the inspector
+        aButtonHelpText.fontSize = 6; // Make the text smaller to fit the NES resolution
+        aButtonHelpText.color = Color.white; // Make the text white
+        aButtonHelpText.alignment = TextAnchor.MiddleLeft;// Align it at the middle left of the text box
+
+        bButtonHelpText.text = "Place building/factory"; // Tell the player what the B button does
+        bButtonHelpText.font = howToPlayFont; // Set this text to use the Nintendo font in the inspector
+        bButtonHelpText.fontSize = 6; // Make the text smaller to fit the NES resolution
+        bButtonHelpText.color = Color.white; // Make the text white
+        bButtonHelpText.alignment = TextAnchor.MiddleLeft;// Align it at the middle left of the text box
     }
 }
